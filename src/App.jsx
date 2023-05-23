f9a5b7e13da2a4f84b4f81df4c224818a8ed684f
AxelHuon <81225848+AxelHuon@users.noreply.github.com>
2023-05-23 09:30:56 +0200

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import "./assets/style/main.css"
-import Home from "./views/home/Home.jsx";
 import Header from "./layouts/Header.jsx";
+import Router from "./router/Router.jsx";
 
 function App() {
 
   return (
     <div className={"content-page"}>
       <Header/>
-      <Home/>
+      <Router/>
     </div>
   )
 }
---
+++ src/router/Router.jsx
@@ -0,0 +1,11 @@
+import Home from "../views/home/Home.jsx";
+
+const Router = () => {
+	
+	
+	return (
+		<Home/>
+	);
+};
+
+export default Router;
