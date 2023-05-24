2be3c51bbb5694ad9c2f79c94b72e7d32c6d6974
AxelHuon <81225848+AxelHuon@users.noreply.github.com>
2023-05-24 11:28:55 +0200

    Instagram button

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,17 +1,22 @@
-import {useRef} from "react";
-import {Expo} from "gsap/gsap-core";
-import gsap from "gsap"
 import {Link} from "react-router-dom";
+import {useLocomotiveScroll} from "react-locomotive-scroll";
 
 const Header = () => {
+	const {scroll} = useLocomotiveScroll()
 	
-	
- 
+ 	const handleScroll = (target) =>{
+		scroll.scrollTo(target)
+	}
 	
 	return (
 		<header className={"header"}>
 			<aside className={"header-container"}>
 				<div className={"header-container-logo"}><Link to={"/"} className={"color-secondary-gradient regular text-30"}>OKULUS</Link></div>
+				<ul className={"header-container-menu"}>
+					<li onClick={(e)=>handleScroll("#about")}><p className={"color-secondary-gradient regular text-16"}>About</p></li>
+					<li onClick={(e)=>handleScroll("#about")}><p className={"color-secondary-gradient regular text-16"}>Gallery</p></li>
+					<li onClick={(e)=>handleScroll("#about")}><p className={"color-secondary-gradient regular text-16"}>Contact</p></li>
+				</ul>
 			</aside>
 		</header>
 	);
--- src/views/home/Partials/HomeAbout.jsx
+++ src/views/home/Partials/HomeAbout.jsx
@@ -1,4 +1,4 @@
-import {SplitChars, Tween} from "react-gsap";
+import {SplitChars, Tween,Reveal} from "react-gsap";
 import {useEffect, useState} from "react";
 import gsap from "gsap"
 import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
@@ -28,10 +28,18 @@ const HomeAbout = () => {
 	setInterval(checkClass, 1000);
 	
 	
-	return (<section data-scroll data-scroll-section data-scroll-offset={"200"} className={"home-about"}>
+	return (<section id={"about"} data-scroll data-scroll-section data-scroll-offset={"300"} className={"home-about"}>
+		<div className={"home-about-social"}>
+			<div className={"home-about-social-item instagram"}>
+				<div data-scroll data-scroll-speed={"2"} data-scroll-direction={"horizontal"} className={"home-about-social-item-container"}>
+				<img src={"images/instagram.svg"}/>
+				<p className={"color-green text-30 regular"}>@balcokulus</p>
+				</div>
+			</div>
+		</div>
 		<div className={"home-about-container"}>
 			<div className={"home-about-container-text"}>
-				<h3 data-scroll data-scroll-speed={"1"} className={"color-white regular text-120"}>
+				<h3 data-scroll data-scroll-speed={"10"} data-scroll-offset={"200"} data-scroll-direction={"horizontal"} className={"color-white regular text-120"}>
 					<Tween playState={playState} from={{y: '100px', opacity: "0", rotate: 20}}
 						   to={{y: '0', opacity: "100%", rotate: 0}}
 						   ease="expo.out()"
@@ -43,12 +51,16 @@ const HomeAbout = () => {
 						</SplitChars>
 					</Tween>
 				</h3>
-				<p className={"color-white text-20 paragraph"} data-scroll data-scroll-speed={"-2"} data-scroll-direction={"horizontal"}> Hi there! I'm a 20-year-old French painter who is
+				<Reveal repeat>
+					<Tween from={{ opacity: 0}} to={{opacity:1}} duration={2}>
+				<p className={"color-white text-20 paragraph"} data-scroll data-scroll-speed={"-1"} data-scroll-direction={"horizontal"}> Hi there! I'm a 20-year-old French painter who is
 					passionate about art and collaborating with French designer brands. I love expressing myself through
 					vibrant colors and bold shapes, creating artworks that captivate the imagination. Working with
 					brands allows me to merge art and fashion, adding a unique artistic dimension to their creations. I
 					draw inspiration from French culture and enjoy exploring new techniques to push the boundaries of my
 					creativity.</p>
+					</Tween>
+				</Reveal>
 			</div>
 			<div className={"home-about-container-image"}>
 				<img data-scroll data-scroll-speed={"3"} data-scroll-direction={"horizontal"} src={"/images/me.png"}/>
