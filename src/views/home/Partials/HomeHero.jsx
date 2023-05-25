211cec8c41eebb3a7120bd916a030656b39d62e4
AxelHuon <81225848+AxelHuon@users.noreply.github.com>
2023-05-25 09:22:02 +0200

    commit

--- src/views/home/Partials/HomeHero.jsx
+++ src/views/home/Partials/HomeHero.jsx
@@ -26,15 +26,14 @@ const HomeHero = () => {
 	
 	
 	useEffect(() => {
-		gsap.to(imgRef.current, {opacity: 1, transition: 1.4, filter: "blur(0px)", scale: 1, ease: Expo.easeIn,delay: 1});
-		gsap.to(textRefIntro.current, {opacity: 1, transition: 1.4, filter: "blur(0px)", scale: 1, ease: Expo.easeIn,delay: 1});
-		gsap.to(persoRef.current, {opacity: 1, transition: 1.4, filter: "blur(0px)", scale: 1, ease: Expo.easeIn,delay: 1});
+		gsap.to(imgRef.current, {opacity: 1, transition: 1.4, filter: "blur(0px)", scale: 1, ease: Expo.easeIn});
+		gsap.to(textRefIntro.current, {opacity: 1, transition: 1.4, filter: "blur(0px)", scale: 1, ease: Expo.easeIn});
+		gsap.to(persoRef.current, {opacity: 1, transition: 1.4, filter: "blur(0px)", scale: 1, ease: Expo.easeIn});
 		
 		
 		const timelineColorText = new gsap.timeline({repeat:true})
 		
-		
-		timelineColorText.to(textRefIntro.current, {color: "greenyellow", duration:3,delay:3}, ">-1");
+		timelineColorText.to(textRefIntro.current, {color: "greenyellow", duration:3}, ">-1");
 		timelineColorText.to(textRefIntro.current, {color: "blue", duration:3},">-1");
 		timelineColorText.to(textRefIntro.current, {color: "red", duration:3},">-1");
 		timelineColorText.to(textRefIntro.current, {color: "#0E87D0", duration:3},">-1");
@@ -43,12 +42,19 @@ const HomeHero = () => {
 	});
 	
 	
+	const handleShake = () =>{
+		const timeline = new gsap.timeline()
+		timeline.to(persoRef.current, {x:10,duration:0.2});
+	}
+	
+	
 	return (<section id={"hero"} data-scroll data-scroll-section className={"home-hero"}>
 			<div className={"home-hero-container"}>
-				<div className={"home-hero-container-perso"}>
-					<img ref={persoRef} data-scroll data-scroll-speed={"-2"} src={"/images/perso.png"}/>
+				<div data-scroll data-scroll-speed={"-2"} className={"home-hero-container-perso"}>
+					<img onClick={(e)=>handleShake()} ref={persoRef} src={"/images/perso.png"}/>
 				</div>
-				<h2 ref={textRefIntro} data-scroll data-scroll-speed={"-2"}   className={"text-200 regular color-primary"}>
+				<div data-scroll data-scroll-speed={"-2"} className={"home-hero-container-text"}>
+				<h2 ref={textRefIntro}  className={"text-200 regular color-primary"}>
 					<Tween playState={statePlay} from={{opacity: "0"}} to={{opacity: "100%"}} ease="expo.out()" duration={3} stagger={0.15}>
 						<SplitChars
 							wrapper={<span style={{display:"inline-block"}}/>}
@@ -57,8 +63,9 @@ const HomeHero = () => {
 						</SplitChars>
 					</Tween>
 				</h2>
-					<div className={"home-hero-container-image"}>
-					<ReactParallaxTilt data-scroll data-scroll-speed={"4"} style={style} reset={true} tiltReverse={true} glareEnable={true} glareReverse={true} glareColor={"#FFF9F0"} scale={1.02} transitionEasing={"cubic-bezier(.03,.98,.52,.99)"} tiltMaxAngleX={10} tiltMaxAngleY={10}>
+				</div>
+					<div data-scroll data-scroll-speed={"4"} className={"home-hero-container-image"}>
+					<ReactParallaxTilt style={style} reset={true} tiltReverse={true} glareEnable={true} glareReverse={true} glareColor={"#FFF9F0"} scale={1.02} transitionEasing={"cubic-bezier(.03,.98,.52,.99)"} tiltMaxAngleX={10} tiltMaxAngleY={10}>
 							<img className={"image-tab"} ref={imgRef} src={"/images/332172514_970661424301110_3590082735548445311_n.webp"}/>
 						</ReactParallaxTilt>
 					</div>
