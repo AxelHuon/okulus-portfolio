e473a4b0000e8c7e06ec3ba8be4bc015de2a14df
Sofian Slimani <70762272+Sev7n2507@users.noreply.github.com>
2023-05-25 14:07:21 +0200

    feat: add Footer

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,25 +1,31 @@
 import React, { useRef } from 'react';
 import { useLocation } from 'react-router-dom';
-import Home from "../views/Home/Home";
-import { Route, Routes } from "react-router-dom";
-import LocoScroll from "../utils/LocoScroll";
-import Header from "../layouts/Header";
+import Home from '../views/Home/Home';
+import { Route, Routes } from 'react-router-dom';
+import LocoScroll from '../utils/LocoScroll';
+import Header from '../layouts/Header';
+import Footer from '../layouts/Footer';
 
 const Router = () => {
-	
-	const ref = useRef();
-	const location = useLocation();
-	
-	return (
-		<LocoScroll innerRef={ref} direction="vertical">
-			<div className={"smooth-scroll"}  data-scroll data-scroll-container ref={ref}>
-				<Header />
-				<Routes>
-					<Route path="/" element={<Home  />} />
-				</Routes>
-			</div>
-		</LocoScroll>
-	);
+  const ref = useRef();
+  const location = useLocation();
+
+  return (
+    <LocoScroll innerRef={ref} direction="vertical">
+      <div
+        className={'smooth-scroll'}
+        data-scroll
+        data-scroll-container
+        ref={ref}
+      >
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <Footer />
+      </div>
+    </LocoScroll>
+  );
 };
 
 export default Router;
